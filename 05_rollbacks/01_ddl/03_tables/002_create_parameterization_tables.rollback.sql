594ddee10b6771886e164d35c3aadfefb1c1aee6
pemarival <mvalenzuelapenagos@gmail.com>
2026-05-18 17:19:34 -0500

    module parameterization

---
+++ 05_rollbacks/01_ddl/03_tables/002_create_parameterization_tables.rollback.sql
@@ -0,0 +1,11 @@
+-- ============================================================================
+-- Parameterization Module Tables - Rollback
+-- Schema: parameterization
+-- ============================================================================
+DROP TABLE IF EXISTS parameterization.employee CASCADE;
+DROP TABLE IF EXISTS parameterization.price CASCADE;
+DROP TABLE IF EXISTS parameterization.payment_method CASCADE;
+DROP TABLE IF EXISTS parameterization.day_type CASCADE;
+DROP TABLE IF EXISTS parameterization.legal_information CASCADE;
+DROP TABLE IF EXISTS parameterization.company CASCADE;
+DROP TABLE IF EXISTS parameterization.client CASCADE;
